0317b37063603029f93e6aa064cd14177b8f5568
Chiien <fuchienhsu12@gmail.com>
2019-03-25 15:42:36 -0300

    Docs() remove .gitignore build folder

---
+++ docs/precache-manifest.9cd733819b05f5642dd793831cd899b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "98f7ba11c0008416714360d79b38fcc3",
+    "url": "/static/media/user.98f7ba11.png"
+  },
+  {
+    "revision": "9e4333c7d8e5a30b69340b9320f22d73",
+    "url": "/static/media/pay_now.9e4333c7.png"
+  },
+  {
+    "revision": "8ac0daf5a4bb9368c5d19a4517d3f90b",
+    "url": "/static/media/paid.8ac0daf5.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0abb6518fae300be652f",
+    "url": "/static/js/main.cc9b226e.chunk.js"
+  },
+  {
+    "revision": "ff0643d9ce9039d01780",
+    "url": "/static/js/2.f84d1deb.chunk.js"
+  },
+  {
+    "revision": "0abb6518fae300be652f",
+    "url": "/static/css/main.ec9c1537.chunk.css"
+  },
+  {
+    "revision": "77cf4e9923b50d9b199dee42a0ecebf2",
+    "url": "/index.html"
+  }
+];
